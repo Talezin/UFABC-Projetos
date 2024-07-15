cb51e360b998e678a944cccf20e9aa32008c2683
Thallis <104874259+Talezin@users.noreply.github.com>
2024-07-15 11:31:19 -0300

    Delete PI directory

--- PI/imc.py
+++
@@ -1,6 +0,0 @@
-Altura = float(input())
-Peso = float(input())
-
-Imc = Peso/(Altura**2)
-
-print(f"{Imc:.2f}")
--- PI/volumePiramide.py
+++
@@ -1,6 +0,0 @@
-L = float(input())
-A = float(input())
-
-Volume = 1/3*((L**2) * A)
-
-print(f"{Volume:.2f}")
